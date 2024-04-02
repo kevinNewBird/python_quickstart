57728bbab866ee7dad6be804eff99502a6c44c87
zhao.song <song0586@126.com>
2024-04-02 17:20:06 +0800

    feat: 递归函数

--- 03_函数编程/代码/10_递归函数.py
+++ 03_函数编程/代码/10_递归函数.py
@@ -1,18 +1,31 @@
 # 阶乘 1*2*3...n!
+lst = list()
+
+
 def fact(n):
     if n == 0:
         return 1
+    lst.append(n)
     return n * fact(n - 1)
 
 
-print(fact(5))
-
+# reverse = lst.reverse()
+ret_fact = fact(5)
+lst.reverse()
+lst = [f"{i}" for i in lst]
+print("表达式:【{}】的计算结果为{}".format(" * ".join(lst), ret_fact))
 
 # 1+2+3...+n
+lst.clear()
+
 def add(n):
     if n == 0:
         return 0
+    lst.append(n)
     return n + add(n - 1)
 
 
-print(add(5))
+ret_add = add(5)
+lst.reverse()
+lst = [f"{i}" for i in lst]
+print("表达式:【{}】的计算结果为{}".format(" + ".join(lst), ret_add))
